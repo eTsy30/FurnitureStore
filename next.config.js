6e2cd4d49bdd52c05e45c4077c19032e9e33c752
tsy <tsy@mail.com>
2023-08-02 15:28:49 +0300

    deploy-3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ignoreBuildErrors: true,
+  ignoreBuildErrors: true,
   images: {
     remotePatterns: [
       {
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
+        {/* <Header /> */}
         <main className="container">{children}</main>
         <Footer />
       </body>
